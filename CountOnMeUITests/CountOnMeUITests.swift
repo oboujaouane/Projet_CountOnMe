eba9657d41365fd76fe4bbc2f7a81d4b03a64ecf
Ousama boujaouane <oboujaouane@gmail.com>
2020-09-02 20:29:00 +0200

    5 UITests finalized

--- CountOnMeUITests/CountOnMeUITests.swift
+++ CountOnMeUITests/CountOnMeUITests.swift
@@ -21,6 +21,43 @@ class CountOnMeUITests: XCTestCase {
         continueAfterFailure = false
     }
 
+    // Check if alertview is showed with message impossible to divide by 0
+    func testGivenIs1DividedBy0_WhenEqualButtonTapped_ThenAlertControllerShouldReturnErrorMessage() {
+        app.buttons["1"].tap()
+        app.buttons["÷"].tap()
+        app.buttons["0"].tap()
+        app.buttons["="].tap()
+
+        // Check if the alert contains the "Impossible de diviser par 0 !" message
+        XCTAssert(app.alerts.element.staticTexts["Impossible de diviser par 0 !"].exists)
+    }
+    
+    // Check if AC button replace text with 0
+    func testGivenIs1Plus1Equal2_WhenACButtonTapped_Then0ShouldBeDisplayed() {
+        app.buttons["AC"].tap()
+
+        let currentTextView = app.textViews["currentTextView"]
+        XCTAssertEqual(currentTextView.value as! String, "0")
+    }
+    
+    // Check if C button remove last entry "= 2"
+    func testGivenIs1Plus1Equal2_WhenCButtonTapped_Then1Plus1ShouldBeDisplayed() {
+        app.buttons["C"].tap()
+
+        let currentTextView = app.textViews["currentTextView"]
+        XCTAssertEqual(currentTextView.value as! String, "1 + 1")
+    }
+    
+    // Result 2 divided by 4 to show a decimal number (0.5)
+    func testGivenIs1Plus1Equal2_WhenDivideResultBy4_Then0Dot5ShouldBeDisplayed() {
+        app.buttons["÷"].tap()
+        app.buttons["4"].tap()
+        app.buttons["="].tap()
+        
+        let currentTextView = app.textViews["currentTextView"]
+        XCTAssertEqual(currentTextView.value as! String, "2 ÷ 4 = 0.5")
+    }
+    
     // Perform a complex operation
     func testGivenIs9MultipliedBy12Plus96DividedBy3Minus4_WhenEqualOperatorButtonTapped_Then136ShouldBeDisplayed() {
         app.buttons["9"].tap()
@@ -39,5 +76,4 @@ class CountOnMeUITests: XCTestCase {
         let currentTextView = app.textViews["currentTextView"]
         XCTAssertEqual(currentTextView.value as! String, "9 × 12 + 96 ÷ 3 - 4 = 136")
     }
-
 }
